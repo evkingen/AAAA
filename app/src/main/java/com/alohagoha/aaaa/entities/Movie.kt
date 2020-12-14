5f3949f41f59d896dd76be00cc02b32d986fa1cb
alohagoha <zlock14@gmail.com>
2020-12-14 20:37:12 +0300

    Добавлена сущность "Movie" используется в MoviesListAdapter для отображения списка с фильмами в FragmentMoviesList. MoviesDataSource - список фильмов.

---
+++ app/src/main/java/com/alohagoha/aaaa/entities/Movie.kt
@@ -0,0 +1,15 @@
+package com.alohagoha.aaaa.entities
+
+import androidx.annotation.DrawableRes
+
+data class Movie(
+        val id: Long,
+        val name: String,
+        val genre: String,
+        val restriction_rating: String,
+        val rating: Int,
+        val countReview: Int,
+        val duration: Long,
+        val isFavorite: Boolean = false,
+        @DrawableRes val imageUrl: Int
+)
