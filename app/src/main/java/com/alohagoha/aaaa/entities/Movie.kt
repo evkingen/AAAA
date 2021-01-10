fb4a41ab9b985244b20dbf7098e4ec3bdb0b0642
alohagoha <zlock14@gmail.com>
2021-01-10 17:27:28 +0300

    Добавлены зависимости kotlin serialization plugin и корутины В assets Добавлены данные о фильмах и актерах в формате json а также парсер данных. Изменена модель.

--- app/src/main/java/com/alohagoha/aaaa/entities/Movie.kt
+++ app/src/main/java/com/alohagoha/aaaa/entities/Movie.kt
@@ -1,15 +1,15 @@
 package com.alohagoha.aaaa.entities
 
-import androidx.annotation.DrawableRes
-
 data class Movie(
-        val id: Long,
-        val name: String,
-        val genre: String,
-        val restriction_rating: String,
-        val rating: Float,
-        val countReview: Int,
-        val duration: Long,
-        val isFavorite: Boolean = false,
-        @DrawableRes val imageUrl: Int
+        val id: Int,
+        val title: String,
+        val overview: String,
+        val poster: String,
+        val backdrop: String,
+        val ratings: Float,
+        val numberOfRatings: Int,
+        val minimumAge: Int,
+        val runtime: Int,
+        val genres: List<Genre>,
+        val actors: List<Actor>
 )
